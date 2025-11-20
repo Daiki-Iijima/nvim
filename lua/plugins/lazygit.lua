daa01608efa2e6a8606007cc33f8def9d1de719c
djima <daiki.applecreate@gmail.com>
2025-11-20 22:56:39 +0900

    feat : lazygitをvim上で開けるように

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = { "LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter" },
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
+    },
+  }
+}
